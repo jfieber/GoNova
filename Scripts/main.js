eadadb833f388f9d458c81e638621197c2040f18
John Fieber <jrf@ursamaris.org>
2021-01-20 11:16:19 -0800

    Add module tidy and vendor tasks

--- Scripts/main.js
+++ Scripts/main.js
@@ -16,10 +16,46 @@ function plog(prefix) {
     };
 }
 
+function createTasks() {
+    let go = gopls.ToolPath('go');
+    if (!go) {
+        console.warn("Couldn't find go executable");
+        return;
+    }
+
+    let tasks = [];
+    let modTidy = new Task('Mod Tidy');
+    modTidy.setAction(
+        Task.Build,
+        new TaskProcessAction(go, {
+            args: ['mod', 'tidy'],
+            env: {},
+        })
+    );
+    tasks.push(modTidy);
+
+    let modVendor = new Task('Mod Vendor');
+    modVendor.setAction(
+        Task.Build,
+        new TaskProcessAction(go, {
+            args: ['mod', 'vendor'],
+            env: {},
+        })
+    );
+    tasks.push(modVendor);
+
+    nova.assistants.registerTaskAssistant({
+        provideTasks: function () {
+            return [modTidy, modVendor];
+        },
+    });
+}
+
 // Language server instance
 var gls = null;
 
 exports.activate = () => {
+    createTasks();
     gls = new GoLanguageServer();
     gls.start().then(plog('activate')).catch(plog('activate warning'));
 };
