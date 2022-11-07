a4a98c8facbd148781d200e61b737492cb3406c9
John Fieber <jrf@ursamaris.org>
2022-11-07 14:03:27 -0800

    tree-sitter highlighting for go.mod

---
+++ GoNova.novaextension/Queries/gomod-highlights.scm
@@ -0,0 +1,23 @@
+(comment) @comment
+
+(module_path) @string
+
+[
+  "go"
+  "module"
+  "exclude"
+  "require"
+  "replace"
+  "retract"
+] @keyword.construct
+
+[
+  "("
+  ")"
+  "["
+  "]"
+] @bracket
+
+[
+  "=>"
+] @operator
