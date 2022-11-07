a23229eb776343f5c3a47d3b1847b9f398868e3b
John Fieber <jrf@ursamaris.org>
2022-11-07 08:42:57 -0800

    Symbols: only match exported symbols
    Indexing private symbols makes project symbol search a mess.

--- GoNova.novaextension/Queries/symbols.scm
+++ GoNova.novaextension/Queries/symbols.scm
@@ -1,17 +1,22 @@
 ((function_declaration name: (identifier) @name) @subtree
+ (#match? @name "^[A-Z]")
  (#set! role function))
 
 ((method_declaration name: (field_identifier) @name) @subtree
+ (#match? @name "^[A-Z]")
  (#set! role method))
 
 ((const_declaration
 	(const_spec name: (identifier) @name) @subtree)
+ (#match? @name "^[A-Z]")
  (#set! role constant))
 
 ((var_declaration
 	(var_spec name: (identifier) @name) @subtree)
+ (#match? @name "^[A-Z]")
  (#set! role variable))
 
 ((type_declaration
 	(type_spec name: (type_identifier) @name) @subtree)
+ (#match? @name "^[A-Z]")
  (#set! role type))
